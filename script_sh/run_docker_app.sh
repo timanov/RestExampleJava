b9ced6c2bb3569908d948c6c87ef4ebd8178f0a6
Mikhail <mihailtimanov@gmail.com>
2022-06-14 08:26:04 +0300

    Добавление скрипта и редактирование докерфайла

--- script_sh/run_docker_app.sh
+++ script_sh/run_docker_app.sh
@@ -1,3 +1,3 @@
 cd rest_example
-docker build -t happymishka/rest_example .
-docker run --name rest_example -p 80:8080 -d happymishka/rest_example
+docker build -t rest_example .
+docker run --name rest_example -p 80:8080 -d rest_example
