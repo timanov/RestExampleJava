6d7ee14d3f89bcf65e59100ae7c2255a67487e08
Mikhail <mihailtimanov@gmail.com>
2022-06-14 08:55:02 +0300

    fix

--- script_sh/git_download_project.sh
+++ script_sh/git_download_project.sh
@@ -2,7 +2,7 @@ cd ~
 git config --global user.email "mihailtimanov@gmail.com"
 git config --global
 git clone https://timanov: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@github.com/timanov/RestExampleJava
-apt -y install openjdk-8-jdk
+apt install openjdk-8-jdk -y
 apt install maven -y
 cd RestExampleJava
 mvn spring-boot:run
