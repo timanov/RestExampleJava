2c15f2492ea68a3d04aa9f3bed25c7bcb252be37
Mikhail <mihailtimanov@gmail.com>
2022-06-14 08:58:43 +0300

    fix

--- script_sh/git_download_project.sh
+++ script_sh/git_download_project.sh
@@ -2,6 +2,7 @@ cd ~
 git config --global user.email "mihailtimanov@gmail.com"
 git config --global
 git clone https://timanov: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@github.com/timanov/RestExampleJava
+apt-get update
 apt install openjdk-8-jdk -y
 apt install maven -y
 cd RestExampleJava
