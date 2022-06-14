babd844e518070fa6a0ba6c6f318ebf9224f3557
Mikhail <mihailtimanov@gmail.com>
2022-06-14 08:39:34 +0300

    Добавление dockefile и документации

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM openjdk:8-jdk-alpine
 
-RUN mvn install
-
 COPY target/docker-rest_example-0.0.1-SNAPSHOT.jar rest_example-0.0.1-SNAPSHOT.jar
 ENTRYPOINT ["java", "-jar", "/rest_example-0.0.1-SNAPSHOT.jar"]
--- script_sh/git_download_project.sh
+++ script_sh/git_download_project.sh
@@ -2,4 +2,8 @@ cd ~
 git config --global user.email "mihailtimanov@gmail.com"
 git config --global
 git clone https://timanov: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@github.com/timanov/RestExampleJava
+sudo apt -y install openjdk-8-jdk
+cd RestExampleJava
+mvn clean
+
 
